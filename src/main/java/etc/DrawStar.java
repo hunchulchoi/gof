b8190e070ae3fd60e70f61e00f6b0edc838a5b9d
hunchulchoi <nettlica@live.co.kr>
2022-06-05 14:42:48 +0900

    draw star

--- src/main/java/etc/DrawStar.java
+++ src/main/java/etc/DrawStar.java
@@ -10,7 +10,7 @@ public class DrawStar {
 
     for(int i=0 ; i<size; i++){
       for(int j=0; j<size; j++){
-        stringBuilder.append((print(size, i, j)?"*":""));
+        stringBuilder.append((print(size, i, j)?"##":"  "));
       }
       stringBuilder.append('\n');
     }
