0cc02b8fda7ba9b72096cf181ab167118bd96674
hunchulchoi <nettlica@live.co.kr>
2022-07-10 16:38:19 +0900

    10. state pattern

--- src/main/java/ch10_state/gatcha/refactoring/State.java
+++ src/main/java/ch10_state/gatcha/refactoring/State.java
@@ -8,9 +8,9 @@ public abstract class State {
     동전있음, 동전없음, 알맹이판매, 알맹이매진, 보너스
   }
 
-  abstract void 동전투입();
-  abstract void 동전반환();
-  abstract void 손잡이돌림();
+  public abstract void 동전투입();
+  public abstract void 동전반환();
+  public abstract void 손잡이돌림();
   public void 리필(){
     throw new IllegalStateException("지금은 리필 할 수 없습니다." + this);
   }
--- src/main/java/ch10_state/gatcha/refactoring/WinningState.java
+++ src/main/java/ch10_state/gatcha/refactoring/WinningState.java
@@ -2,17 +2,17 @@ package ch10_state.gatcha.refactoring;
 
 public class WinningState extends State{
   @Override
-  void 동전투입() {
+  public void 동전투입() {
     throw new IllegalStateException("지금은 동전 투입이 힘듭니다.");
   }
 
   @Override
-  void 동전반환() {
+  public void 동전반환() {
     throw new IllegalStateException("지금은 동전 반환이 안됩니다");
   }
 
   @Override
-  void 손잡이돌림() {
+  public void 손잡이돌림() {
     System.out.println("당첨되었습니다. 상품이 하나더 나옵니다.");
   }
 }
